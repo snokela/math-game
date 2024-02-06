f7d50f4b5a05e67ad0879e3d927e438f4fa76648
saananokela <smnokela@gmail.com>
2024-02-06 14:00:31 +0200

    oikeiden ja väärien vastausten laskutoiminnot lisätty js-tiedostoon

--- js/functions.js
+++ js/functions.js
@@ -2,6 +2,9 @@
 
 let rand_num1 = 0
 let rand_num2 = 0
+// määritellään muuttujat vielä oikealle ja väärälle vastaus-määrälle
+let correct = 0
+let incorrect = 0
 
 const getRandomIntNumberInRange = (min,max) => {
     return Math.floor(Math.random() * max) + min;
@@ -23,8 +26,13 @@ document.querySelector('button').addEventListener('click', () => {
     const correctAnswer = rand_num1 + rand_num2
     if (answer === correctAnswer) {
         alert('Correct!')
+        correct += 1
+        // console.log(correct)
+
     } else {
         alert('Incorrect!')
+        incorrect += 1
+        // console.log(incorrect)
     }
 
     randomizeNumbers()
